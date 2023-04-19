f0f80c36a594028a7ea792289536729d937dec9a
Taegwan Youn <tkyoun0421@naver.com>
2023-04-19 19:01:07 +0900

    CHH03_07. 페이징 구현하기

--- src/app.js
+++ src/app.js
@@ -3,10 +3,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
-
+const store = {
+  currentPage: 1,
+};
 
 function getData(url) {
   ajax.open('GET', url, false);
@@ -16,17 +15,17 @@ function getData(url) {
 }
 const ul = document.createElement('ul');
 
-function newsFeed() {
+function newsFeed() {  
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
 
   newsList.push('<ul>');
 
-  for(let i = 0; i < 10; i++) {
+  for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(
       `
       <li>
-        <a href="${newsFeed[i].id}">
+        <a href="#/show/${newsFeed[i].id}">
           ${newsFeed[i].title} (${newsFeed[i].comments_count});
         </a>
       </li>
@@ -35,21 +34,26 @@ function newsFeed() {
   }
   
   newsList.push('</ul>');
-
+  newsList.push(`
+    <div>
+      <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+      <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
+    </div>
+  `)
   container.innerHTML = newsList.join('');
 }
 
 function newsDetail() {
-  const id = location.hash.substr(1);
-  console.log('newsDetail');
+  const id = location.hash.substr(7);
   const newsContent = getData(CONTENT_URL.replace('@id', id));
 
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
+
     <div>
-      <a href="#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
-  `
+  `;
 }
 
 function router() {
@@ -57,11 +61,14 @@ function router() {
 
   if (routePath === '') {
     newsFeed();
+  } else if (routePath.indexOf('#/page/') >= 0){
+    store.currentPage = Number(routePath.substr(7));
+    newsFeed();
   } else {
     newsDetail(); 
   }
 }
 
-window.addEventListener('hashchange', newsDetail);
+window.addEventListener('hashchange', router);
 
 router();
